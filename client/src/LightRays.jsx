bff8d93471cce72707afb9df211f1d5c59664c33
Abhay <abhaymaurya821@gmail.com>
2025-08-03 19:04:40 +0530

    save

--- client/src/LightRays.jsx
+++ client/src/LightRays.jsx
@@ -1,6 +1,6 @@
 import { useRef, useEffect, useState } from "react";
 import { Renderer, Program, Triangle, Mesh } from "ogl";
-import "./lightRays.css";
+import "./src/light.css";
 
 
 const DEFAULT_COLOR = "#ffffff";
